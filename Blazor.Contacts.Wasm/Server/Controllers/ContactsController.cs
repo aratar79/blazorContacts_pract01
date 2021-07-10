9cf5334acb36f7fa5dab76f8425f09db4ec47524
Jordi Z <aratar79@gmail.com>
2021-07-10 09:54:38 +0200

    add Contacts controller with insert and update methods

--- Blazor.Contacts.Wasm/Server/Controllers/ContactsController.cs
+++ Blazor.Contacts.Wasm/Server/Controllers/ContactsController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Shared;
 
 namespace Server.Controllers
 {
@@ -6,9 +9,36 @@ namespace Server.Controllers
     [ApiController]
     public class ContactsController : ControllerBase
     {
-        public ContactsController()
+        private readonly IContactRepository _contactRepository;
+        public ContactsController(IContactRepository contactRepository)
         {
-            
+            _contactRepository = contactRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertContact([FromBody]Contact contact) 
+        {
+            if (contact == null) return BadRequest();
+            if (string.IsNullOrEmpty(contact.FirstName)) ModelState.AddModelError("FirstName", "First name can't be empty");
+            if (string.IsNullOrEmpty(contact.LastName)) ModelState.AddModelError("LastName", "Last name can't be empty");
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            await _contactRepository.InsertContact(contact);
+
+            return CreatedAtAction(nameof(InsertContact), new { id = contact.Id }, contact);         
+        }
+
+        [HttpPut("{id}")]    
+        public async Task<IActionResult> UpdateContact(int id, [FromBody]Contact contact) 
+        {
+            if (contact == null) return BadRequest();
+            if (string.IsNullOrEmpty(contact.FirstName)) ModelState.AddModelError("FirstName", "First name can't be empty");
+            if (string.IsNullOrEmpty(contact.LastName)) ModelState.AddModelError("LastName", "Last name can't be empty");
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            await _contactRepository.UpdateContact(contact);
+
+            return CreatedAtAction(nameof(InsertContact), new { id = contact.Id }, contact);         
         }
     }
 }
