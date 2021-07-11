2abc25affe75d9954806555cbc3901122668a86c
Jordi Z <aratar79@gmail.com>
2021-07-11 17:40:00 +0200

    [200~we start with the services~

--- Blazor.Contacts.Wasm/Client/Services/ContactService.cs
+++ Blazor.Contacts.Wasm/Client/Services/ContactService.cs
@@ -1,7 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shared;
+
 namespace Client.Services
 {
     public class ContactService : IContactService
     {
-        
+        public Task DeleteContact(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contact>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Contact> GetDetails(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SaveContact(Contact contact)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Blazor.Contacts.Wasm/Client/Services/IContactService.cs
+++ Blazor.Contacts.Wasm/Client/Services/IContactService.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shared;
+
 namespace Client.Services
 {
     public interface IContactService
     {
-         
+         Task SaveContact(Contact contact);
+         Task DeleteContact(int id);
+         Task <IEnumerable<Contact>> GetAll();
+         Task<Contact> GetDetails(int id);
     }
 }
