599f1ae11af0f19e96ccbd976f92024c40963067
aratar79 <aratar79@gmail.com>
2021-07-11 19:40:32 +0200

    Implement services

--- Blazor.Contacts.Wasm/Client/Services/ContactService.cs
+++ Blazor.Contacts.Wasm/Client/Services/ContactService.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Shared;
 
@@ -6,24 +8,19 @@ namespace Client.Services
 {
     public class ContactService : IContactService
     {
-        public Task DeleteContact(int id)
-        {
-            throw new System.NotImplementedException();
-        }
+        private readonly HttpClient _httpClient;
 
-        public Task<IEnumerable<Contact>> GetAll()
+        public ContactService(HttpClient httpClient)
         {
-            throw new System.NotImplementedException();
+            _httpClient = httpClient;
         }
-
-        public Task<Contact> GetDetails(int id)
+        public async Task DeleteContact(int id) => await _httpClient.DeleteAsync($"api/contacts/{id}");
+        public async Task<IEnumerable<Contact>> GetAll() => await _httpClient.GetFromJsonAsync<IEnumerable<Contact>>($"api/contacts");
+        public async Task<Contact> GetDetails(int id) => await _httpClient.GetFromJsonAsync<Contact>($"api/contacts/{id}");
+        public async Task SaveContact(Contact contact)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public Task SaveContact(Contact contact)
-        {
-            throw new System.NotImplementedException();
+            if (contact.Id == 0)  await _httpClient.PostAsJsonAsync<Contact>($"api/contacts", contact);
+            else await _httpClient.PutAsJsonAsync<Contact>($"api/contacts/{contact.Id}", contact);       
         }
     }
 }
